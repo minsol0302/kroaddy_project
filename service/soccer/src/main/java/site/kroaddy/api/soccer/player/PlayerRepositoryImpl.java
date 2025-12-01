f66577b0cee567658c13cc2f7a58e5ec2496a9c1
feuzw <feuzw21@gmail.com>
2025-12-02 07:21:50 +0900

    파일구조

--- service/soccer/src/main/java/site/kroaddy/api/soccer/player/PlayerRepositoryImpl.java
+++
@@ -1,9 +0,0 @@
-package site.kroaddy.api.soccer.player;
-
-import lombok.RequiredArgsConstructor;
-import com.querydsl.jpa.impl.JPAQueryFactory;
-
-@RequiredArgsConstructor
-public class PlayerRepositoryImpl implements PlayerRepositoryCustom {
-    private final JPAQueryFactory queryFactory;
-}
